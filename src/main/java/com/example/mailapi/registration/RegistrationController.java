ff1709f457ce8c0dac8f491e73e6c7c1a23feb2b
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-07-13 05:51:40 +0900

    add jpa and fix registrationservice error

--- src/main/java/com/example/mailapi/registration/RegistrationController.java
+++ src/main/java/com/example/mailapi/registration/RegistrationController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class RegistrationController {
 
+    private Registrationservice registrationService;
+
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
--- src/main/java/com/example/mailapi/registration/Registrationservice.java
+++ src/main/java/com/example/mailapi/registration/Registrationservice.java
@@ -1,5 +1,8 @@
 package com.example.mailapi.registration;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Registrationservice {
 
     public String register(RegistrationRequest request){
