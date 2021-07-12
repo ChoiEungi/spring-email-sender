2228e2aa2259723a925059fdd6130a281eb635f8
ChoiEungi <choieungi@gm.gist.ac.kr>
2021-07-13 05:59:16 +0900

    add postmapping

--- src/main/java/com/example/mailapi/registration/RegistrationController.java
+++ src/main/java/com/example/mailapi/registration/RegistrationController.java
@@ -1,6 +1,7 @@
 package com.example.mailapi.registration;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ public class RegistrationController {
 
     private Registrationservice registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
